07a4c29f3cfb78aa92b8c5f4c4d36a9a074a02e8
chelsea cheung <chelseatcheung@gmail.com>
2015-12-16 10:14:53 -0800

    minor changes

---
+++ config.js
@@ -0,0 +1,2 @@
+window._APIKEY = '74f79c67e773f3a1bb29ae4d2cecc50b';
+window._APPID = 'd2272e81';
--- request-handler.js
+++ request-handler.js
@@ -1,6 +1,6 @@
 var APIKEY = '74f79c67e773f3a1bb29ae4d2cecc50b';
 var APPID = 'd2272e81';
-var searchRecipe = 'http://api.yummly.com/v1/api/recipes?_app_id=' + APPID + '&_app_key=' + APIKEY + '&q=';
+searchRecipe = 'http://api.yummly.com/v1/api/recipes?_app_id=' + APPID + '&_app_key=' + APIKEY + '&q=';
 angular.module('services', [])
 
 .factory('Links', function($http) {
